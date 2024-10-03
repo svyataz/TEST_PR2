af5ccfeba6fc0b035b5274251ea2829bdec93f4a
svyataz <svyataz@gmail.com>
2024-10-03 15:04:34 +0300

    done

---
+++ src/Lists.java
@@ -0,0 +1,98 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Lists {
+    public static void main(String[] args) {
+        int[] MyList = {12, 6, 413, 1, 153, 10, 10, 10, 10, 232343, 6454, 4, 10, 12312, 10};
+        System.out.println(Arrays.toString(quickSort(MyList)));
+        System.out.println(Arrays.toString(bubbleSort(MyList)));
+        System.out.println(counter(MyList, 10));
+        System.out.println(maxArray(MyList));
+        System.out.println(Arrays.toString(toBinary(MyList)));
+    }
+    public static String[] toBinary(int[] MyList){
+        int maxLen = 0;
+        int n = MyList.length;
+        String[] bin = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            bin[i] = Integer.toBinaryString(MyList[i]);
+            maxLen = Math.max(maxLen, bin[i].length());
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (bin[i].length() != maxLen)
+                bin[i] = String.format("%" + maxLen + "s", bin[i]).replace(' ', '0').replace(' ', '0');
+        }
+        return bin;
+    }
+
+    public static int counter(int[] MyList, int x){
+        int c = 0;
+        for (int i = 1; i < MyList.length; i++) {
+            if (MyList[i] == x) {
+                c++;
+            }
+        }
+        return c;
+    }
+
+    public static int maxArray(int[] a) {
+
+        int max = a[0]; // saves a bit of time
+
+        for (int j = 1; j < a.length; j++) {
+            if (a[j] > max) {
+                max = a[j];
+            }
+
+        }
+        return max;
+    }
+
+    public static int[] bubbleSort(int[] MyList){
+        for (int i = 0; i < MyList.length - 1; i++) {
+            for(int j = 0; j < MyList.length - i - 1; j++) {
+                if(MyList[j + 1] < MyList[j]) {
+                    int swap = MyList[j];
+                    MyList[j] = MyList[j + 1];
+
+                    MyList[j + 1] = swap;
+                }
+            }
+        }
+        return MyList;
+    }
+
+    public static int[] quickSort(int[] MyList) {
+        return quickSort(MyList, 0, MyList.length - 1);
+    }
+
+    public static int[] quickSort(int[] MyList, int low, int high) {
+
+        if (MyList.length == 0 || low >= high) return MyList;
+
+        int middle = low + (high - low) / 2;
+        int border = MyList[middle];
+        int i = low, j = high;
+
+        while (i <= j) {
+            while (MyList[i] < border) i++;
+            while (MyList[j] > border) j--;
+            if (i <= j) {
+                int swap = MyList[i];
+                MyList[i] = MyList[j];
+                MyList[j] = swap;
+                i++;
+                j--;
+            }
+        }
+
+        if (low < j) quickSort(MyList, low, j);
+        if (high > i) quickSort(MyList, i, high);
+        return MyList;
+    }
+}
+
+
---
+++ src/Strings.java
@@ -0,0 +1,54 @@
+public class Strings {
+    public static void main(String[] args) {
+        String str = "Aasdfasdfsdaf_sddfsafsdFsd5334fsefsddf_fsfsdsdff_fsdfFsdfFFF";
+        System.out.println(palindrome(str));
+        System.out.println(counter(str, "f"));
+        System.out.println(isAlpha(str));
+        System.out.println(isDigit(str));
+        System.out.println(splitting(str));
+    }
+
+    public static Boolean palindrome(String str){
+        String buff = new StringBuilder(str.substring((int)Math.ceil(str.length() / 2.0), str.length())).reverse().toString();
+        if(str.substring(0, str.length() / 2).equals(buff)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static int counter(String str, String x){
+        return str.length() - str.replace(x, "").length();
+    }
+
+    public static boolean isAlpha(String str) {
+        char[] chars = str.toCharArray();
+        for (char i : chars) {
+            if(!Character.isLetter(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isDigit(String str) {
+        char[] chars = str.toCharArray();
+        for (char i : chars) {
+            if(!Character.isDigit(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static String splitting(String str){
+        String buff = "";
+        for ( char i :  str.toCharArray()){
+            if (i == Character.toUpperCase(i) && !buff.isEmpty() && Character.isLetter(i)) {
+                buff += " ";
+            }
+            if (i == '_') i = ' ';
+            buff += i;
+        }
+        return buff;
+    }
+}
